4611369f366ecc46020d6da37a64c767afac4866
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-09-01 13:42:36 +0100

    Update

--- lib/providers/CarsProvider.dart
+++ lib/providers/CarsProvider.dart
@@ -19,45 +19,45 @@ class CarsProvider extends BaseProvider {
             'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
         name: 'Model S',
       ),
-      Car(
-        backgroungImage: 'images/bg1.png',
-        images: [
-          'images/tesla1.png',
-          'images/tesla2.png',
-          'images/tesla3.webp',
-          'images/tesla4.png',
-          'images/tesla5.png',
-        ],
-        description:
-            'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
-        name: 'Model S',
-      ),
-      Car(
-        backgroungImage: 'images/bg1.png',
-        images: [
-          'images/tesla1.png',
-          'images/tesla2.png',
-          'images/tesla3.webp',
-          'images/tesla4.png',
-          'images/tesla5.png',
-        ],
-        description:
-            'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
-        name: 'Model S',
-      ),
-      Car(
-        backgroungImage: 'images/bg1.png',
-        images: [
-          'images/tesla1.png',
-          'images/tesla2.png',
-          'images/tesla3.webp',
-          'images/tesla4.png',
-          'images/tesla5.png',
-        ],
-        description:
-            'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
-        name: 'Model S',
-      ),
+      // Car(
+      //   backgroungImage: 'images/modely.png',
+      //   images: [
+      //     'images/tesla1.png',
+      //     'images/tesla2.png',
+      //     'images/tesla3.webp',
+      //     'images/tesla4.png',
+      //     'images/tesla5.png',
+      //   ],
+      //   description:
+      //       'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
+      //   name: 'Model S',
+      // ),
+      // Car(
+      //   backgroungImage: 'images/models.png',
+      //   images: [
+      //     'images/tesla1.png',
+      //     'images/tesla2.png',
+      //     'images/tesla3.webp',
+      //     'images/tesla4.png',
+      //     'images/tesla5.png',
+      //   ],
+      //   description:
+      //       'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
+      //   name: 'Model S',
+      // ),
+      // Car(
+      //   backgroungImage: 'images/model3.png',
+      //   images: [
+      //     'images/tesla1.png',
+      //     'images/tesla2.png',
+      //     'images/tesla3.webp',
+      //     'images/tesla4.png',
+      //     'images/tesla5.png',
+      //   ],
+      //   description:
+      //       'choosing a model s is a statement. It is a declaration that only the \nabsolute best in quality and performance will do',
+      //   name: 'Model S',
+      // ),
     ];
   }
 }

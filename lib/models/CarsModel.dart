ed70b3ea2b7aa707db22c7b8b3e1eb5a10afa627
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-09-04 13:45:10 +0100

    Update

--- lib/models/CarsModel.dart
+++ lib/models/CarsModel.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/material.dart';
+
 class Car {
   String name;
   String description;
   String backgroungImage;
+  Color color;
   List<String> images;
 
   Car({
+    this.color,
     this.name,
     this.description,
     this.backgroungImage,
